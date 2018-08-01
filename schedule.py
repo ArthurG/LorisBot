e841d1bc59fa8ef72f9c7088cc9f88d5aa416a9d
ArthurG <arthur.guo@uwaterloo.ca>
2018-08-01 01:40:26 -0400

    Change courses to notify

--- schedule.py
+++ schedule.py
@@ -72,12 +72,16 @@ def notify_if_needed(course_data, good_sections):
                 .format(sec, course_data[sec]["remaining"], course_data[sec]["actual"])})
 
 def main():
-    course_data = get_course_details("Fall 2018", "Business", "481")
-    good_sections = ["Business Policy I - 56 - BU 481 - C", "Business Policy I - 1021 - BU 481 - G", "Business Policy I - 1660 - BU 481 - K"]
-    notify_if_needed(course_data, good_sections)
+    #course_data = get_course_details("Fall 2018", "Business", "481")
+    #good_sections = ["Business Policy I - 56 - BU 481 - C", "Business Policy I - 1021 - BU 481 - G", "Business Policy I - 1660 - BU 481 - K"]
+    #notify_if_needed(course_data, good_sections)
 
     course_data = get_course_details("Spring 2019", "Business", "491")
     good_sections = ["Business Policy II - 71 - BU 491 - P", "Business Policy II - 72 - BU 491 - Q"]
+    notify_if_needed(course_data, good_sections)
+
+    course_data = get_course_details("Spring 2019", "Business", "493U")
+    good_sections = ["Fin Markets & Security Trading - 480 - BU 493U - H"]
     notify_if_needed(course_data, good_sections)
 
 if __name__ == "__main__":
